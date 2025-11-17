54ecc8e2e999aabc3dc22fa8a206cd66aca468cf
Yannik Caspar <y.caspar@bluewin.ch>
2025-11-17 15:17:08 +0100

    take screenshot

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -15,7 +15,7 @@ class QuestionsTest < ApplicationSystemTestCase
     visit ask_url
     fill_in "question", with: "Hello"
     click_on "Ask"
-
+    take_screenshot
     assert_text "I don't care, get dressed and go to work!"
   end
 end
